659da84c65b02261d49890a81f61d5952ccaa685
intratable <85131627+intratable@users.noreply.github.com>
2022-06-18 09:36:41 -0300

    Detect-IP-ON v1.0

--- IP_ON.sh
+++ IP_ON.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-   
-for i in $(seq 1 254); do
-     timeout 1 bash -c "ping -c 1 0.0.0.$i" &> /dev/null && echo "[+]Host  0.0.0.$i - ACTIVE"&
-done; wait
+for i in $(seq 1 254); do timeout 1 bash -c "ping -c 1 0.0.0.$i" &> /dev/null && echo "[+]Host  0.0.0.$i - ACTIVE"& done; wait
